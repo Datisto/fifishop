c0472cc8bf1dd29ae417b292dd1b7bdcb98f5966
Datisto <nickyettikiller@gmail.com>
2025-10-20 17:31:33 +0300

    Updated AdminLogin.tsx

--- src/pages/admin/AdminLogin.tsx
+++ src/pages/admin/AdminLogin.tsx
@@ -63,7 +63,7 @@ export default function AdminLogin() {
                   onChange={(e) => setUsername(e.target.value)}
                   required
                   className="w-full pl-11 pr-4 py-3 border border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
-                  placeholder="NazarFL5492"
+                  placeholder="Login"
                 />
               </div>
             </div>
